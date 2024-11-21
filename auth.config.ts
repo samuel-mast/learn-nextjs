c6d3ae90dd8187940f1bd01a0201bc01b61bb88e
Samuel Mast <samuelmst@gmail.com>
2024-11-21 12:40:44 -0600

    Reverted Redirect Change

--- auth.config.ts
+++ auth.config.ts
@@ -12,8 +12,7 @@ export const authConfig = {
                 if (isLoggedIn) return true;
                 return false;
             } else if (isLoggedIn) {
-                //return Response.redirect(new URL('/dashboard', nextUrl));
-                return Response.redirect(new URL('/dashboard'));
+                return Response.redirect(new URL('/dashboard', nextUrl));
             }
             return true;
         },
